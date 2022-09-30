e99abe5991a4bd8ffb5ee79f8ed82f1902c9a48c
Rahul Aggarwal <raggarwal43@gatech.edu>
2022-09-29 21:26:47 -0400

    add entry point

---
+++ src/slam.cpp
@@ -0,0 +1,12 @@
+#include "optimizer.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "state_estimation");
+
+
+    Optimizer optimizer;
+
+    // single-threaded
+    ros::spin();
+    return 0;
+}
