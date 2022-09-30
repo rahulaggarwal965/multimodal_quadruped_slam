dc3fa81c66af76bdb8bb08ba90348f5426b1f25f
Rahul Aggarwal <raggarwal43@gatech.edu>
2022-09-30 00:37:18 -0400

    optimizer now owns imu

--- include/optimizer.h
+++ include/optimizer.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <ros/publisher.h>
@@ -5,8 +7,8 @@
 
 #include "nav_msgs/Path.h"
 
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <gtsam/nonlinear/ISAM2.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
@@ -16,7 +18,12 @@
 #include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/base/Matrix.h>
 #include <gtsam/linear/NoiseModel.h>
+#include <gtsam/navigation/ImuFactor.h>
 
+#include "imu.h"
+
+// NOTE(Rahul):
+// might fold in IMU into this class to avoid complexity
 struct Optimizer {
     gtsam::ISAM2 isam;
     gtsam::NonlinearFactorGraph graph;
@@ -25,19 +32,21 @@ struct Optimizer {
 
     ros::NodeHandle nh;
 
-    ros::Subscriber imu_sub;
+    // TODO(Rahul):
+    // we include the IMU inside this class because it exchanges significant 
+    // information between global optimization and IMU preintegration
+    // perhaps it would be better to encapsulate IMU information inside a ros msg
+    // but it is not clear how I would do that right now
+    IMU imu;
     ros::Publisher imu_bias_pub;
 
     ros::Publisher trajectory_pub;
 
-    tf::TransformListener transform_listener;
-    tf::TransformBroadcaster transform_broadcaster;
+    tf2_ros::Buffer transform_buffer;
+    tf2_ros::TransformListener transform_listener;
+    tf2_ros::TransformBroadcaster transform_broadcaster;
 
-    Optimizer() {
-        
-        /* imu_sub = nh.subscribe("imu", 5, &Optimizer::imu_callback, this); */
-        trajectory_pub  = nh.advertise<nav_msgs::Path>("trajectory", 1);
-    }
+    Optimizer();
 
     // TODO(rahul): think about when we want to actually optimize. Ideally, this
     // should not happen very often, preferably when we get a lidar/camera scan.
--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -1,6 +1,17 @@
 #include "optimizer.h"
 #include "utils.h"
 
+Optimizer::Optimizer() :
+    transform_listener(transform_buffer)
+{
+    trajectory_pub  = this->nh.advertise<nav_msgs::Path>("trajectory", 1);
+}
+
+// TODO:
+// we need to optimize at some point. I am thinking
+// that when we receive an exteroceptive factor (via lidar/camera)
+// we take the latest imu_factor in the threaded queue 
+
 void Optimizer::optimize(int steps) {
 
     this->isam.update(this->graph, this->initial_estimate);
@@ -12,6 +23,12 @@ void Optimizer::optimize(int steps) {
     
     this->graph = gtsam::NonlinearFactorGraph();
     this->initial_estimate.clear();
+
+    this->publish_trajectory();
+
+    // TODO(Rahul):
+    // publish map_to_odom transform
+    // requires listening for odom_to_base_link
 }
 
 void Optimizer::publish_trajectory() {
