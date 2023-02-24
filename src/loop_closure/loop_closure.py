d08834cb9d22ea838bcdc3b572273868170332b1
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2023-02-24 06:21:50 -0500

    initial pass for loop closure integration, thoughts about feature dimensionality reduction of the resnet output

---
+++ src/loop_closure/loop_closure.py
@@ -0,0 +1,56 @@
+import rospy
+from cv_bridge import CvBridge
+# import pykdtree
+
+from sensor_msgs.msg import Image
+
+from LoopClosureDL import LoopClosureDL
+
+import numpy as np
+import torch
+
+class LoopClosure:
+
+    def __init__(self) -> None:
+
+        self.rgb_sub = rospy.Subscriber("/camera/color", Image, callback=self.handle_rgb)
+        self.depth_sub = rospy.Subscriber("/camera/depth", Image, callback=self.handle_depth)
+
+        self.model = LoopClosureDL()
+        self.bridge = CvBridge()
+
+        # TODO(rahul): find a better way of synchronizing these topics
+        self.rgb_frames = 0
+        self.depth_frames = 0
+
+    def handle_rgb(self, image : Image):
+        self.rgb = np.asarray(self.bridge.imgmsg_to_cv2(image), dtype=np.uint8)
+        self.rgb_frames += 1
+        if self.rgb_frames == self.depth_frames:
+            self.handle_rgbd()
+        pass
+
+
+    def handle_depth(self, depth : Image):
+        self.rgb = np.asarray(self.bridge.imgmsg_to_cv2(depth), dtype=np.uint8)
+        self.depth_frames += 1
+        if self.rgb_frames == self.depth_frames:
+            self.handle_rgbd()
+
+    def handle_rgbd(self):
+        # size of 2048
+        # NOTE(rahul): because we are doing these forward passes incrementally, we can't actually
+        # think about using PCA to reduce a space of feature vectors on to a subset of the eigenbasis
+        # of the covariance. However, matching in a 2048 feature dim space is probably quite inefficient
+        # for long horizon trajectories (especially in a robot operating in the real world.)
+        # Need to think about how we can solve this problem, especially because kdtrees suffer greatly from 
+        # high dimensional spaces. Perhaps we can use an ANN (see: opencv FLANN) approximate nearest neighbors
+        # or we could train an encoder/decoder to reduce the dimensionality of the feature vectors themselves.
+        # much work needs to be done to think about this. I think, for now, we can probably get a way with using
+        # a kdtree
+        f = self.model.compute_hook_activation(torch.from_numpy(self.rgb).unsqueeze(0)).squeeze();
+
+
+
+
+
