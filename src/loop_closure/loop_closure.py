52d20f0cf3ec66702c552120e4c16875c7f027b9
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2023-02-24 06:31:35 -0500

    add basic timekeeping for rgbd handling later in c

--- src/loop_closure/loop_closure.py
+++ src/loop_closure/loop_closure.py
@@ -16,6 +16,8 @@ class LoopClosure:
         self.rgb_sub = rospy.Subscriber("/camera/color", Image, callback=self.handle_rgb)
         self.depth_sub = rospy.Subscriber("/camera/depth", Image, callback=self.handle_depth)
 
+        #self.feature_vector_pub = rospy.Publisher("/camera/feature_vector", FeatureVector, )
+
         self.model = LoopClosureDL()
         self.bridge = CvBridge()
 
@@ -23,8 +25,12 @@ class LoopClosure:
         self.rgb_frames = 0
         self.depth_frames = 0
 
+        self.rgb_stamp = rospy.Time.now();
+        self.depth_stamp = rospy.Time.now();
+
     def handle_rgb(self, image : Image):
         self.rgb = np.asarray(self.bridge.imgmsg_to_cv2(image), dtype=np.uint8)
+        self.rgb_stamp = image.header.stamp
         self.rgb_frames += 1
         if self.rgb_frames == self.depth_frames:
             self.handle_rgbd()
@@ -32,7 +38,8 @@ class LoopClosure:
 
 
     def handle_depth(self, depth : Image):
-        self.rgb = np.asarray(self.bridge.imgmsg_to_cv2(depth), dtype=np.uint8)
+        self.depth = np.asarray(self.bridge.imgmsg_to_cv2(depth), dtype=np.uint8)
+        self.depth_stamp = depth.header.stamp
         self.depth_frames += 1
         if self.rgb_frames == self.depth_frames:
             self.handle_rgbd()
@@ -48,7 +55,9 @@ class LoopClosure:
         # or we could train an encoder/decoder to reduce the dimensionality of the feature vectors themselves.
         # much work needs to be done to think about this. I think, for now, we can probably get a way with using
         # a kdtree
+        # ALSO take a look at ResNetVAE https://github.com/hsinyilin19/ResNetVAE
         f = self.model.compute_hook_activation(torch.from_numpy(self.rgb).unsqueeze(0)).squeeze();
+        # self.feature_vector_pub.publish(f)
 
 
 
